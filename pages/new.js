88688dc330d96535bd1917147af36421ee1b9b27
Anup Joseph <arunjoseph3007@gmail.com>
2022-08-31 10:40:14 +0530

    Create new repo page finished

--- pages/new.js
+++ pages/new.js
@@ -1,28 +1,48 @@
+import { LockIcon } from "@/icons/lock";
+import { PublicIcon } from "@/icons/public";
 import { useState } from "react";
 
 export default function CreateRepoPage() {
   const [repoName, setRepoName] = useState("");
   const [repoDesc, setRepoDesc] = useState("");
   const [isPublic, setIsPublic] = useState(true);
+  const [error, setError] = useState(null);
 
+  //@ Submit handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  //@ Handle change
+  const handleChange = (e) => {
+    setRepoName(e.target.value);
+
+    setError(!e.target.validity.valid);
+  };
+
+  //$ UI
   return (
-    <div className="w-full max-w-[1000px] mx-auto">
-      <h1>Create a Repository</h1>
+    <div className="w-full max-w-[800px] mx-auto mt-[5rem]">
+      <h1 className="text-3xl my-2">Create a Repository</h1>
       <p>
         A repository contains all project files, including the revision history.
       </p>
-      {isPublic ? "hey" : "hi"}
-      <hr />
-      <form className="form-control ">
+      <hr className="my-2" />
+
+      {/* //? Form */}
+      <form onSubmit={handleSubmit} className="form-control ">
         <label htmlFor="repo-name-input" className="label">
           Repository name
         </label>
         <input
           value={repoName}
-          onChange={(e) => setRepoName(e.target.value)}
+          onChange={handleChange}
           id="repo-name-input"
           type="text"
-          className="input input-bordered"
+          pattern="[a-zA-Z0-9_]{4,32}"
+          title="A string consisting alphabets, numbers and characters - or _ with length not more than 32 characters and not less than 4 characters"
+          className="input input-secondary input-bordered invalid:input-error"
+          required
         />
         <label htmlFor="desc-input" className="label">
           Description
@@ -36,18 +56,19 @@ export default function CreateRepoPage() {
         />
 
         {/* //@ Radio buttons */}
-        <div>
+        <div className="my-4">
           <div className="flex items-center gap-3 mb-3">
             <input
               type="radio"
-              name="radio-1"
+              name="radio-public"
               className="radio"
               checked={isPublic}
               onChange={(e) => setIsPublic(e.target.checked)}
             />
+            <PublicIcon />
             <label>
               <h4>Public</h4>
-              <p>
+              <p className="text-sm text-secondary">
                 Anyone on the internet can see this repository. You choose who
                 can commit.
               </p>
@@ -56,17 +77,24 @@ export default function CreateRepoPage() {
           <div className="flex items-center gap-3 mb-3">
             <input
               type="radio"
-              name="radio-1"
+              name="radio-private"
               className="radio"
               checked={!isPublic}
               onChange={(e) => setIsPublic(!e.target.checked)}
             />
+            <LockIcon />
             <label>
               <h4>Private</h4>
-              <p>You choose who can see and commit to this repository.</p>
+              <p className="text-sm text-secondary">
+                You choose who can see and commit to this repository.
+              </p>
             </label>
           </div>
         </div>
+
+        <button className="btn" type="submit">
+          Create Repository
+        </button>
       </form>
     </div>
   );
