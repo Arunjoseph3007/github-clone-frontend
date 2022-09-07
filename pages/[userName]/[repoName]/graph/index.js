97de7c9ace27948b2a7b14b0ac1cf0b9b727239a
Anup Joseph <arunjoseph3007@gmail.com>
2022-09-07 19:09:18 +0530

    Graph screen new approach trying

--- pages/[userName]/[repoName]/graph/index.js
+++ pages/[userName]/[repoName]/graph/index.js
@@ -1,5 +1,6 @@
 import MainRepoLayout from "@/layouts/MainRepoLayout";
-import { gitGraph } from "@/utils/gitGraph";
+// import { gitGraph } from "@/utils/gitGraph";
+import { gitGraphSvg } from "@/utils/gitGraphSvg";
 import { useRouter } from "next/router";
 
 const formatGraph = (graph) => (
@@ -19,7 +20,7 @@ export default function GraphPage({ data }) {
     <div>
       <div className="p-3 w-full max-w-[1000px] mx-auto">
         <div className="bg-neutral-focus p-4 font-mono mockup-code">
-          {data
+          {/* {data
             .split("\n")
             .filter((a) => a)
             .map((line, i, arr) => {
@@ -40,7 +41,19 @@ export default function GraphPage({ data }) {
                   <code className="text-sm ml-3">{text && text}</code>
                 </pre>
               );
-            })}
+            })} */}
+          {data.map((node, i, arr) => (
+            <pre
+              className="w-full"
+              style={{ color: `hsl(${(360 * i) / arr.length}, 50%, 65%)` }}
+              data-prefix={i}
+              key={i}
+            >
+              <code>{node.hash}</code>||
+              <code className="text-sm ml-3">{node.message}</code>||
+              <code className="text-sm ml-3">{node?.parent}</code>
+            </pre>
+          ))}
         </div>
       </div>
     </div>
@@ -52,7 +65,8 @@ GraphPage.getLayout = MainRepoLayout;
 export const getServerSideProps = async (ctx) => {
   const pathName = `${ctx.params.userName}/${ctx.params.repoName}`;
 
-  const { data, error } = gitGraph(pathName);
+  // const { data, error } = gitGraph(pathName);
+  const { data, error } = gitGraphSvg(pathName);
 
   if (error) {
     return {
