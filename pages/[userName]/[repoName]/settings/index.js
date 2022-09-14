b00c4bcc5747237020c4007ffe55ace26db7677f
Anup Joseph <arunjoseph3007@gmail.com>
2022-09-14 10:16:59 +0530

    Single child bug fix attempt

--- pages/[userName]/[repoName]/settings/index.js
+++ pages/[userName]/[repoName]/settings/index.js
@@ -1,7 +1,11 @@
 import MainRepoLayout from "@/layouts/MainRepoLayout";
 
 export default function SettingsPage() {
-  return <div>settings page</div>;
+  return (
+    <div>
+      <h1>settings page</h1>
+    </div>
+  );
 }
 
 SettingsPage.getLayout = MainRepoLayout;
