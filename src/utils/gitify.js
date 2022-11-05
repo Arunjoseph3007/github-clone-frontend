4713a1fc0d1aefdb66d4915769ff39ef26bdf03c
Anup Joseph <arunjoseph3007@gmail.com>
2022-11-05 18:25:18 +0530

    Changed gitify function to support all OS's

--- src/utils/gitify.js
+++ src/utils/gitify.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const rootUserDir = process.env.GIT_ROOT_DIRECTORY;
 
-export const gitify = (repo) => rootUserDir + repo + ".git";
+export const gitify = (repo) => path.normalize(rootUserDir + repo + ".git")
