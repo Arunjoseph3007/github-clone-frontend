cab42928a31a9c174d46187e46e9733699a7ecdc
Arunjoseph3007 <arunjoseph3007@gmail.com>
2023-07-24 12:01:38 +0530

    Highlighting back to auto

--- src/libs/highlight.js
+++ src/libs/highlight.js
@@ -1,9 +1,9 @@
 import hljs from "highlight.js/lib/common";
 
 export const syntaxHighlight = (text, extension) => {
-  try {
-    return hljs.highlight(text, { language: extension }).value;
-  } catch (e) {
+  // try {
+  //   return hljs.highlight(text, { language: extension }).value;
+  // } catch (e) {
     return hljs.highlightAuto(text).value;
-  }
+  // }
 };
